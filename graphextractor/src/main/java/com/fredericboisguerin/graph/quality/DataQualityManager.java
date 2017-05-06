2b823ba11a3712112ab65dd9f38fb65b4377224d
Fred <frederic.boisguerin@gmail.com>
2017-05-06 14:10:47 +0200

    Remove useless project graph-extractor

--- graphextractor/src/main/java/com/fredericboisguerin/graph/quality/DataQualityManager.java
+++
@@ -1,7 +0,0 @@
-package com.fredericboisguerin.graph.quality;
-
-@FunctionalInterface
-public interface DataQualityManager<T> {
-
-    T cleanData(T data);
-}
--- graphextractor/src/main/java/com/fredericboisguerin/graph/quality/DrawLinesDataQualityManager.java
+++
@@ -1,6 +0,0 @@
-package com.fredericboisguerin.graph.quality;
-
-import com.fredericboisguerin.graph.model.input.DrawLines;
-
-public interface DrawLinesDataQualityManager extends DataQualityManager<DrawLines> {
-}
