7d6dd0d7bbd79c90cecb5482be650626b866faf4
Fred <frederic.boisguerin@gmail.com>
2017-05-09 13:53:16 +0200

    Fix bug in detection of vertical grid

--- project/swing-app/src/main/java/com/fredericboisguerin/wrapper/DrawLineHelper.java
+++ project/swing-app/src/main/java/com/fredericboisguerin/wrapper/DrawLineHelper.java
@@ -1,29 +1,28 @@
 package com.fredericboisguerin.wrapper;
 
-import com.fredericboisguerin.pdf.parser.model.DrawingPoint;
-import com.fredericboisguerin.pdf.drawlines.model.DrawLine;
-
 import java.util.function.Function;
+
+import com.fredericboisguerin.pdf.drawlines.model.DrawLine;
+import com.fredericboisguerin.pdf.parser.model.DrawingPoint;
 
 public class DrawLineHelper {
 
     private final CoordComparator coordComparator = new CoordComparator();
 
     public boolean isVertical(DrawLine drawLine) {
-        return drawLine.hasAtLeast(3) && isCoordTheSameForAll(drawLine, DrawingPoint::getX);
-    }
-
-    public boolean isSerie(DrawLine drawLine) {
-        return hasAtLeastTwoPoints(drawLine) && !isVertical(drawLine) && !isHorizontal(
-                drawLine);
+        return hasAtLeastTwoPoints(drawLine) && isCoordTheSameForAll(drawLine, DrawingPoint::getX);
     }
 
     public boolean isHorizontal(DrawLine drawLine) {
         return hasAtLeastTwoPoints(drawLine) && isCoordTheSameForAll(drawLine, DrawingPoint::getY);
     }
 
+    public boolean isSerie(DrawLine drawLine) {
+        return drawLine.hasAtLeast(3) && !isVertical(drawLine) && !isHorizontal(drawLine);
+    }
+
     private boolean isCoordTheSameForAll(DrawLine drawLine,
-                                         Function<DrawingPoint, Float> coordGetter) {
+            Function<DrawingPoint, Float> coordGetter) {
         DrawingPoint firstPoint = drawLine.getFirstPoint();
         float coord = coordGetter.apply(firstPoint);
         return areAllCoordsEqualTo(drawLine, coord, coordGetter);
@@ -35,9 +34,9 @@ public class DrawLineHelper {
     }
 
     private boolean areAllCoordsEqualTo(DrawLine drawLine, float coord,
-                                        Function<DrawingPoint, Float> coordGetter) {
-        return drawLine.allPointsMatch(drawingPoint -> areCoordEqual(coord, coordGetter
-                .apply(drawingPoint)));
+            Function<DrawingPoint, Float> coordGetter) {
+        return drawLine.allPointsMatch(
+                drawingPoint -> areCoordEqual(coord, coordGetter.apply(drawingPoint)));
     }
 
     private static boolean hasAtLeastTwoPoints(DrawLine drawLine) {
