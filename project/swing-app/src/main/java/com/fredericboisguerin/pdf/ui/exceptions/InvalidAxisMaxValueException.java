18331917f2056afc20013e98020bfb4e4fc0d0f6
Fred <frederic.boisguerin@gmail.com>
2017-05-17 22:34:57 +0200

    Do not support Swing application anymore #RIP

--- project/swing-app/src/main/java/com/fredericboisguerin/pdf/ui/exceptions/InvalidAxisMaxValueException.java
+++
@@ -1,7 +0,0 @@
-package com.fredericboisguerin.pdf.ui.exceptions;
-
-public class InvalidAxisMaxValueException extends InvalidInputException {
-    public InvalidAxisMaxValueException() {
-        super("axis max");
-    }
-}
--- project/swing-app/src/main/java/com/fredericboisguerin/pdf/ui/exceptions/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package com.fredericboisguerin.pdf.ui.exceptions;
-
-public class InvalidInputException extends Exception {
-
-    public InvalidInputException(String inputId) {
-        super(String.format("InvalidInputException: %s", inputId));
-    }
-}
